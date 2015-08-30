c99429648c44eee63fb258caeaacea6a1e70775b
Manuel Zumbado <manu_z3@hotmail.com>
2015-08-30 11:22:51 -0600

    Eliminado Temporizador_divisor agregado Divisor_Frecuencia y Temporizador por aparte

--- Divisor_Frecuencia.v
+++ Divisor_Frecuencia.v
@@ -1,30 +1,15 @@
 //Se define que el tiempo en que la puerta estar abierta como maximo es de 10seg
 
-//Modulo del temporizador
-module Temporizador_Divisor (
+//Modulo del divisor de frecuencia
+module Divisor_Frecuencia (
 		input  C_100Mhz, //Clock de la fpga = 100MHz
-		input  startTimer,
-		input  restart,
 		output reg C_1Hz
-		/*,
-		output reg timeExpired   
-		*/
 	);
-/*
-	reg timeExpired; //1 expirado, 0 no expirado
-	reg C_1Hz = 1;  //Seal de salida (<em>Se debe asignar un estado lgico</em>).
-	*/ 
-	reg[31:0] contador = 0; //Variable Contador equivale a 25 millones de estados. 
+
+	reg[31:0] contador = 0; //Variable que almacena Contador
 	 
 	always @(posedge C_100Mhz)
 		begin
-			if (startTimer)
-				begin
-				if(restart) //reinicia
-					begin
-					contador <= 0;
-					C_1Hz <= 1'b0;
-					end
 				if (contador == 100000000)          //Si ha cuenta 1s enciende led
 					begin
 					C_1Hz <= 1'b1;
@@ -38,6 +23,5 @@ module Temporizador_Divisor (
 					begin
 					contador <= contador + 1;        //suma contador
 				   end
-				end
 	  end
 endmodule
