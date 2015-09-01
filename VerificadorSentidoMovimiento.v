57c33a20157e46bdd03c9032ab7f591f5c8fba8e
Arturo <artsaldel@hotmail.com>
2015-09-01 00:41:05 -0600

    listo verificador

--- VerificadorSentidoMovimiento.v
+++ VerificadorSentidoMovimiento.v
@@ -40,48 +40,98 @@ module VerificadorSentidoMovimiento(
 		
 		input [2:0] piso_actual;
 		input [1:0] solicitud_ps, solicitud_p1, solicitud_p2, solicitud_p3, solicitud_p4;
-		output [1:0] accion;
+		output reg [1:0] accion;
 		
 		always @(accion)
 			begin
-				if (piso actual == 3'b000)
+				//acciones para el sotano
+				if (piso_actual == 3'b000)
 					begin
 						if (solicitud_ps == 2'bx1) //No realiza accion
 							accion <= 2'b00;
-						if (solicitud_ps == 2'x0)
+						if (solicitud_ps == 2'bx0)
 							begin
 							accion <= 2'b10;
 							accion <= 2'b01;
 							end
-						if (solicitud_p1 == 2'x0 | solicitud_p2 == 2'x0 | solicitud_p3 == 2'x0 | solicitud_p4 == 2'x0)
+						if (solicitud_p1 == 2'bx0 | solicitud_p2 == 2'bx0 | solicitud_p3 == 2'bx0 | solicitud_p4 == 2'bx0)
 							accion <= 2'b10;
 					end
 					
-				else if (piso actual == 3'b001)
+				//acciones para el piso1
+				else if (piso_actual == 3'b001)
 					begin
-						
-					end
-					
-				else if (piso actual == 3'b010)
-					begin
-				 
-					end
-					
-				else if (piso actual == 3'b011)
-					begin
-				 
-					end
-					
-				else if (piso actual == 3'b100)
-					begin
-						if (solicitud_p4 == 2'b1x) //No realiza accion
-							accion <= 2'b00;
-						if (solicitud_p4 == 2'0x)
+						if (solicitud_p1 == 2'bxx)
+							accion <= 01;
+						if (solicitud_ps == 2'bx1)
 							begin
 							accion <= 2'b11;
 							accion <= 2'b01;
 							end
-						if (solicitud_ps == 2'0x | solicitud_p1 == 2'0x | solicitud_p2 == 2'0x | solicitud_p3 == 2'0x)
+						if (solicitud_p2 == 2'b1x)
+							begin
+							accion <= 2'b10;
+							accion <= 2'b01;
+							end
+						if (solicitud_p3 == 2'b1x | solicitud_p4 == 2'b1x)
+							accion <= 2'b10;
+						else if (solicitud_p3 == 2'bx1 | solicitud_p4 == 2'bx1)
+							accion <= 2'b11;
+					end
+					
+				//acciones para el piso2
+				else if (piso_actual == 3'b010)
+					begin
+						if (solicitud_p2 == 2'bxx)
+							accion <= 01;
+						if (solicitud_p1 == 2'bx1)
+							begin
+							accion <= 2'b11;
+							accion <= 2'b01;
+							end
+						if (solicitud_p3 == 2'b1x)
+							begin
+							accion <= 2'b10;
+							accion <= 2'b01;
+							end
+						if (solicitud_ps == 2'b1x | solicitud_p3 == 2'b1x)
+							accion <= 2'b10;
+						else if (solicitud_ps == 2'bx1 | solicitud_p3 == 2'bx1)
+							accion <= 2'b11;
+					end
+				
+				//acciones para el piso3
+				else if (piso_actual == 3'b011)
+					begin
+						if (solicitud_p3 == 2'bxx)
+							accion <= 01;
+						if (solicitud_p2 == 2'bx1)
+							begin
+							accion <= 2'b11;
+							accion <= 2'b01;
+							end
+						if (solicitud_p4 == 2'b1x)
+							begin
+							accion <= 2'b10;
+							accion <= 2'b01;
+							end
+						if (solicitud_ps == 2'b1x | solicitud_p1 == 2'b1x)
+							accion <= 2'b10;
+						else if (solicitud_ps == 2'bx1 | solicitud_p1 == 2'bx1)
+							accion <= 2'b11;
+					end
+				
+				//acciones para el piso 4
+				else if (piso_actual == 3'b100)
+					begin
+						if (solicitud_p4 == 2'b1x) //No realiza accion
+							accion <= 2'b00;
+						if (solicitud_p4 == 2'b0x)
+							begin
+							accion <= 2'b11;
+							accion <= 2'b01;
+							end
+						if (solicitud_ps == 2'b0x | solicitud_p1 == 2'b0x | solicitud_p2 == 2'b0x | solicitud_p3 == 2'b0x)
 							accion <= 2'b11;
 					end
 			end
